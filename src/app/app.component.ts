1192198a674200190c78761d8573102dc4839d2e
rockwang <rockwang@localhost.localdomain>
2019-02-20 11:11:14 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ws-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'warpstor-frontend';
+}
